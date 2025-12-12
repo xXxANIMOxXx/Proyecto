a320e25f450afcd808b22107806e23b464dcda4d
xXxANIMOxXx <juanjosevilaverde@gmail.com>
2025-12-12 20:32:49 +0100

    Node

---
+++ docker/andalucia/dockerfiles/node/admin/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e 
+
+load_entrypoint_nginx(){
+    bash /root/admin/nginx/admin/start.sh
+}
+
+workdir(){
+    cd /root/admin/node/proyectos/proyecto
+}
+
+dependencias(){
+    npm install
+    npm run build
+    cp -r dist/* /var/www/html/
+}
+
+
+nginxreload(){
+    service nginx reload
+}
+
+nginxservice(){
+    exec nginx -g "daemon off;"
+}
+
+main(){
+    load_entrypoint_nginx
+    workdir
+    dependencias
+    nginxreload
+    tail -f /dev/null
+}
+
+main
