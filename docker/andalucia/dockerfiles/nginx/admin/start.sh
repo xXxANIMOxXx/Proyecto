dc8278635e0d260daf53ea1bacb5aa49fa03ec80
xXxANIMOxXx <juanjosevilaverde@gmail.com>
2025-12-12 20:00:38 +0100

    Nginx

---
+++ docker/andalucia/dockerfiles/nginx/admin/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+config_nginx() {
+    nginx &
+}
+
+load_entrypoint_base(){
+    bash /root/admin/base/start.sh
+}
+
+main(){
+ load_entrypoint_base
+ config_nginx
+    # Mantener el contenedor en ejecución
+    tail -f /dev/null
+}
+
+main
