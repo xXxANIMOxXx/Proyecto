bc4b207d907a7211bebd0eb4088617f828b9f5ac
xXxANIMOxXx <juanjosevilaverde@gmail.com>
2025-12-15 20:02:28 +0100

    Cambio en el home

--- docker/andalucia/proyectos/proyecto/pokedex-rutas/src/pages/Home.jsx
+++ docker/andalucia/proyectos/proyecto/pokedex-rutas/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export function Home() {
             <h2>Inicio</h2>
             <p>
                 Bienvenido/a a la Pokedex con rutas. Usa la barra de navegación para ver
-                10 Pokémon aleatorios de cada generación.
+                10 Pokémon aleatorios de cada generación. :D
             </p>
         </div>
     )
